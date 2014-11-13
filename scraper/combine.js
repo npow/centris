f8ffbc65ad4e4a4fde44cc66761c6a35d68020ee
npow <nissan.pow@gmail.com>
2014-11-13 08:55:12 -0500

    combine responses

---
+++ scraper/combine.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+var json2csv = require('json2csv');
+
+var L = [];
+for (var i = 1; i <= 3668; ++i) {
+  data = JSON.parse(fs.readFileSync('responses/' + i + '.json').toString())
+  L = L.concat(data.d.Result);
+}
+console.log(L.length);
+json2csv({data: L, fields: Object.keys(L[0]) }, function(err, csv) {
+  if (err) console.log(err);
+  fs.writeFile('data/listings.csv', csv, function(err) {
+    if (err) throw err;
+    console.log('DONE');
+  });
+});
