137fe187070380d568a71dfd39e198c0f5617988
npow <nissan.pow@gmail.com>
2014-11-19 16:55:59 -0500

    fetch extra data from REMAX

--- scraper/fetch_details.js
+++ scraper/fetch_details.js
@@ -1,26 +1,79 @@
-var fs = require('fs');
+var Promise = require('bluebird');
+var csv = Promise.promisifyAll(require('fast-csv'));
+var fs = Promise.promisifyAll(require('fs'));
+var jsdom = Promise.promisifyAll(require('jsdom'));
+var json2csv = require('json2csv');
+var sys = Promise.promisifyAll(require('sys'));
 
-var H = {};
+var LISTINGS_FILE = 'data/listings.csv';
+var mls_ids = [];
+var L = [];
+var stream = fs.createReadStream(LISTINGS_FILE);
+var csvStream = csv({ headers: true })
+    .on("data", function(data){
+      mls_ids.push('MT' + data.MlsNumber);
+    })
+    .on("end", function() {
+      var sequencer = Promise.resolve();
+      mls_ids.forEach(function (id, i) {
+        sequencer = sequencer.then(function () {
+          return fetchRemax(id);
+        });
+      });
+      sequencer.then(function () {
+        console.log('DONE');
+        json2csv({ data: L, fields: Object.keys(L[0]) }, function(err, csv) {
+          if (err) console.log(err);
+          fs.writeFile('data/extra_data.csv', csv, function(err) {
+            if (err) throw err;
+            console.log('DONE');
+          });
+        });
+      });
+    });
+stream.pipe(csvStream);
 
-for (var i = 1; i <= 3668; ++i) {
-  var data = JSON.parse(fs.readFileSync('responses/' + i + '.json').toString());
-  data.d.Result.forEach(function (x) {
-    var url = x.PasserelleUrl;
-    if (url.length) {
-      var m = url.match(/CodeDest=(.+)&NoMLS=/);
-      if (m.length > 1) {
-        var dest = m[1];
-        H[dest] = H[dest] || 0;
-        H[dest] += 1;
+function fetchRemax(id) {
+  //var id = 'MT28773784';
+  console.log(id);
+  var url = 'http://www.remax-quebec.com/en/MLSRedirect.rmx?sia=' + id;
+  return jsdom.envAsync(url, ["http://code.jquery.com/jquery.js"])
+    .then(function (window) {
+      var $ = window.$;
+      var fields = {
+        'Year built :': 'YearBuilt',
+        'Garage :': 'Garage',
+        'Floor Covering :': 'FloorCovering',
+        'Area :': 'Area',
+        'Zoning :': 'Zoning',
+        'Parking :': 'Parking',
+        'Proximity :': 'Proximity',
+        'Municipal (': 'MunicipalTax',
+        'School (': 'SchoolTax',
+        'Municipal assessment': 'MunicipalAssessment',
+        'Insurance :': 'Insurance',
+        'Heating Energy :': 'HeatingEnergy',
+        'Water Supply :': 'WaterSupply',
+        'Sewage System :': 'SewageSystem',
+        'View :': 'View',
+        'Topography :': 'Topography',
+        'Living area :': 'LivingArea',
+        'Pool :': 'Pool',
+      };
+      var data = { MlsNumber: id.substring(2) };
+      for (var field in fields) {
+        var key = fields[field];
+        data[key] = '';
+        var res = $('td:contains("' + field + '")');
+        if (res && res.next()) {
+          data[key] = res.next().text();
+        }
       }
-    }
-  });
+      if (Object.keys(data).length === 1) {
+        L.push(data);
+      }
+    });
 }
-
-var sortable = [];
-for (var dest in H) sortable.push([dest, H[dest]]);
-sortable.sort(function(a, b) {return a[1] - b[1]});
-console.log(sortable);
 
 /*
 [ [ 'YANNDERY', 1 ],
@@ -116,3 +169,26 @@ console.log(sortable);
   [ 'EGPTECH', 2394 ],
   [ 'REMAX', 3408 ] ]
 */
+function dumpStats() {
+  var H = {};
+
+  for (var i = 1; i <= 3668; ++i) {
+    var data = JSON.parse(fs.readFileSync('responses/' + i + '.json').toString());
+    data.d.Result.forEach(function (x) {
+      var url = x.PasserelleUrl;
+      if (url.length) {
+        var m = url.match(/CodeDest=(.+)&NoMLS=/);
+        if (m.length > 1) {
+          var dest = m[1];
+          H[dest] = H[dest] || 0;
+          H[dest] += 1;
+        }
+      }
+    });
+  }
+
+  var sortable = [];
+  for (var dest in H) sortable.push([dest, H[dest]]);
+  sortable.sort(function(a, b) {return a[1] - b[1]});
+  console.log(sortable);
+}
