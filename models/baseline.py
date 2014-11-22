95d319dcdc94a76535988c458d6d6b71d8ab3aa0
npow <nissan.pow@gmail.com>
2014-11-22 16:54:49 -0500

    use pipeline

--- models/baseline.py
+++ models/baseline.py
@@ -21,20 +21,30 @@ from sklearn.preprocessing import *
 from sklearn.svm import *
 from sklearn.tree import *
 from sklearn import metrics
+np.random.seed(42)
 
 data = pd.read_csv('data/modified_listings.csv')
 Y = data[['BuyPrice']].values
 
 data.drop(['MlsNumber', 'Lat', 'Lng', 'BuyPrice'], axis=1, inplace=True)
 X = np.array(data)
-print X.shape
-print Y.shape
-print X
+print 'X.shape: ', X.shape
+print 'Y.shape: ', Y.shape
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, random_state=42)
+# filter rows with NaN
+X_trunc = X[~np.isnan(X).any(axis=1)]
+Y_trunc = Y[~np.isnan(X).any(axis=1)]
+Y_trunc = Y_trunc.reshape(Y_trunc.shape[0])
+print 'X_trunc.shape: ', X_trunc.shape
+print 'Y_trunc.shape: ', Y_trunc.shape
 
-svr = SVR(C=10000.0)
-svr.fit(X_train, Y_train)
-preds = svr.predict(X_test).astype(float)
+X_train, X_test, Y_train, Y_test = train_test_split(X_trunc, Y_trunc, test_size=0.1, random_state=42)
 
-print mean_squared_error(preds, Y_test)
+clf = Pipeline([
+    ('scaler', StandardScaler()),
+    ('svm', SVR(C=10000.0))
+])
+clf.fit(X_train, Y_train)
+preds = clf.predict(X_test).astype(float)
+
+print 'MSE: ', metrics.mean_squared_error(preds, Y_test)
