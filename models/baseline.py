e31fd720b3ba336cf80730ce2fa4c8d26797966f
npow <nissan.pow@gmail.com>
2014-11-23 23:09:58 -0500

    breakdown by category; remove outliers

--- models/baseline.py
+++ models/baseline.py
@@ -25,27 +25,50 @@ from sklearn import metrics
 np.random.seed(42)
 
 data = pd.read_csv('data/modified_listings.csv')
-Y = data[['BuyPrice']].values
 
-data.drop(['MlsNumber', 'Lat', 'Lng', 'BuyPrice'], axis=1, inplace=True)
-X = np.array(data)
-print 'X.shape: ', X.shape
-print 'Y.shape: ', Y.shape
+for col in ['COP', 'AP', 'LS', 'MA']:#, 'PPR', '2X', '3X', '4X', '5X', 'AU', 'UNI', 'MEM']:
+  print "*" * 80
+  print col
+  X = data[['WalkScore', 'NbPieces', 'NbChambres', 'NbSallesEaux', 'NbSallesBains', 'NbFoyerPoele', 'NbEquipements', 'NbGarages', 'NbStationnements', 'NbPiscines', 'NbBordEaux']]
+  Y = data[['BuyPrice']]
 
-# filter rows with NaN
-X_trunc = X[~np.isnan(X).any(axis=1)]
-Y_trunc = Y[~np.isnan(X).any(axis=1)]
-Y_trunc = Y_trunc.reshape(Y_trunc.shape[0])
-print 'X_trunc.shape: ', X_trunc.shape
-print 'Y_trunc.shape: ', Y_trunc.shape
+  # filter rows with NaN
+  mask = ~np.isnan(X).any(axis=1)
+  X = X[mask]
+  Y = Y[mask]
 
-X_train, X_test, Y_train, Y_test = train_test_split(X_trunc, Y_trunc, test_size=0.1, random_state=42)
+  # remove high-end listings
+  mask = Y['BuyPrice'] < 10**6
+  X = X[mask]
+  Y = Y[mask]
 
-clf = Pipeline([
-    ('scaler', StandardScaler()),
-    ('svm', SVR(C=10000.0))
-])
-clf.fit(X_train, Y_train)
-preds = clf.predict(X_test).astype(float)
+  # remove low-end listings
+  mask = Y['BuyPrice'] > 10**5
+  X = X[mask]
+  Y = Y[mask]
 
-print 'RMSE: ', math.sqrt(metrics.mean_squared_error(preds, Y_test))
+  print "mean: ", Y['BuyPrice'].mean()
+  print "median: ", Y['BuyPrice'].median()
+  print "std: ", Y['BuyPrice'].std()
+
+  # remove outliers
+  mask = np.abs(Y['BuyPrice']-Y['BuyPrice'].median()) <= (3*Y['BuyPrice'].std())
+  X = X[mask]
+  Y = Y[mask]
+
+  X = np.array(X[data[col]==1])
+  Y = np.array(Y[data[col]==1])
+  print 'X.shape: ', X.shape
+  print 'Y.shape: ', Y.shape
+
+  Y = Y.reshape(Y.shape[0])
+  X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, random_state=42)
+
+  clf = Pipeline([
+      ('scaler', StandardScaler()),
+      ('svm', SVR())
+  ])
+  clf.fit(X_train, Y_train)
+  preds = clf.predict(X_test).astype(float)
+
+  print 'RMSE: ', math.sqrt(metrics.mean_squared_error(preds, Y_test))
