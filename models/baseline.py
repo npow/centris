1a96c5514d86688f7763c67f69256f4042e6c71a
npow <nissan.pow@gmail.com>
2014-11-25 16:47:30 -0500

    calc corr, %diff, etc

--- models/baseline.py
+++ models/baseline.py
@@ -9,6 +9,7 @@ import numpy as np
 import pandas as pd
 import scipy
 import sys
+from scipy.stats.stats import pearsonr
 from sklearn.base import BaseEstimator, TransformerMixin
 #from nolearn.dbn import DBN
 from sklearn.cross_validation import *
@@ -25,14 +26,14 @@ from sklearn.tree import *
 from sklearn import metrics
 np.random.seed(42)
 
-USE_CALIBRATION = True
+USE_CALIBRATION = False
 GENERATE_PLOTS = False
 
 data = pd.read_csv('data/modified_listings.csv')
 extra_data = pd.read_csv('data/post_extra_data.csv')
 extra_data = extra_data[['MlsNumber','LivingArea']]
 merged = pd.merge(data, extra_data, on='MlsNumber', suffixes=['_left', '_right'])
-merged.to_csv('all_data.csv')
+merged.to_csv('data/all_data.csv')
 for col in ['AP']:#, 'COP', 'AP', 'LS', 'MA']:#, 'PPR', '2X', '3X', '4X', '5X', 'AU', 'UNI', 'MEM']:
   print "*" * 80
   print col
@@ -77,58 +78,72 @@ for col in ['AP']:#, 'COP', 'AP', 'LS', 'MA']:#, 'PPR', '2X', '3X', '4X', '5X', 
   Y = np.array(Y)
   Y = Y.reshape(Y.shape[0])
   skf = KFold(n=X.shape[0], n_folds=10, shuffle=True, random_state=42)
-  L = []
+  L_rmse = []
+  L_corr = []
+  L_diff = []
   for train_indices, test_indices in skf:
+    print '=' * 80
     X_train, Y_train = X[train_indices], Y[train_indices]
     X_test, Y_test = X[test_indices], Y[test_indices]
 
-    if USE_CALIBRATION:
-      scaler = StandardScaler()
-      X_train = scaler.fit_transform(X_train)
-      X_test = scaler.transform(X_test)
+    for clf_type in [GradientBoostingRegressor]:#[AdaBoostRegressor, BaggingRegressor, BayesianRidge, ElasticNet, ExtraTreesRegressor, GradientBoostingRegressor, KNeighborsRegressor, Lasso, LinearRegression, PassiveAggressiveRegressor, RandomForestRegressor, Ridge, SVR]:
+      if USE_CALIBRATION:
+        scaler = StandardScaler()
+        X_train_scaled = scaler.fit_transform(X_train)
+        X_test_scaled = scaler.transform(X_test)
 
-      clf1 = RandomForestRegressor()
-      clf1.fit(X_train, Y_train)
-      clf1_train = clf1.predict(X_train)
-      clf1_test = clf1.predict(X_test)
+        clf1 = clf_type()
+        clf1.fit(X_train_scaled, Y_train)
+        clf1_train = clf1.predict(X_train_scaled)
+        clf1_test = clf1.predict(X_test_scaled)
 
-      clf1_train = clf1_train.reshape((clf1_train.shape[0],1))
-      clf1_test = clf1_test.reshape((clf1_test.shape[0],1))
+        clf1_train = clf1_train.reshape((clf1_train.shape[0],1))
+        clf1_test = clf1_test.reshape((clf1_test.shape[0],1))
 
-      clf1_train = np.concatenate([clf1_train, X_train], axis=1)
-      clf1_test = np.concatenate([clf1_test, X_test], axis=1)
+        clf1_train = np.concatenate([clf1_train, X_train_scaled], axis=1)
+        clf1_test = np.concatenate([clf1_test, X_test_scaled], axis=1)
 
-      clf = GradientBoostingRegressor()
-      clf.fit(clf1_train, Y_train)
-      preds = clf.predict(clf1_test).astype(float)
-    else:
-      clf = Pipeline([
-         ('scaler', StandardScaler()),
-         # ('clf', AdaBoostRegressor()),
-         # ('clf', ARDRegression()),
-         # ('clf', BaggingRegressor()),
-         # ('clf', BayesianRidge()),
-         # ('clf', ElasticNet()),
-         # ('clf', ExtraTreesRegressor()),
-          ('clf', GradientBoostingRegressor()),
-         # ('clf', KNeighborsRegressor(n_neighbors=5)),
-         # ('clf', Lasso()),
-         # ('clf', LinearRegression()),
-         # ('clf', PassiveAggressiveRegressor()),
-         # ('clf', RandomForestRegressor()),
-         # ('clf', Ridge(alpha=0.5, normalize=False)),
-         # ('clf', SVR()),
-      ])
-      clf.fit(X_train, Y_train)
-      preds = clf.predict(X_test).astype(float)
+        clf = clf_type()
+        clf.fit(clf1_train, Y_train)
+        preds = clf.predict(clf1_test).astype(float)
+      else:
+        clf = Pipeline([
+           ('scaler', StandardScaler()),
+           # ('clf', AdaBoostRegressor()),
+           # ('clf', ARDRegression()),
+           # ('clf', BaggingRegressor()),
+           # ('clf', BayesianRidge()),
+           # ('clf', ElasticNet()),
+           # ('clf', ExtraTreesRegressor()),
+            ('clf', GradientBoostingRegressor()),
+           # ('clf', KNeighborsRegressor(n_neighbors=5)),
+           # ('clf', Lasso()),
+           # ('clf', LinearRegression()),
+           # ('clf', PassiveAggressiveRegressor()),
+           # ('clf', RandomForestRegressor()),
+           # ('clf', Ridge(alpha=0.5, normalize=False)),
+           # ('clf', SVR()),
+        ])
+        clf.fit(X_train, Y_train)
+        preds = clf.predict(X_test).astype(float)
 
-    rmse = math.sqrt(metrics.mean_squared_error(preds, Y_test))
-    print rmse
-    L.append(rmse)
+      rmse = math.sqrt(metrics.mean_squared_error(preds, Y_test))
+      corr = pearsonr(preds, Y_test)
+      diff = np.array([abs(p-a)/a for (p,a) in zip(preds,Y_test)]).mean()
+      
+      print str(clf_type), rmse, corr
+      L_rmse.append(rmse)
+      L_corr.append(corr[0])
+      L_diff.append(diff)
 
-    if GENERATE_PLOTS:
-      plt.plot(Y_test, preds, 'ro')
-      plt.show()
-      break
-  print "Mean RMSE: ", np.array(L).mean()
+      if GENERATE_PLOTS:
+        plt.plot(Y_test, preds, 'ro')
+        plt.show()
+        break
+        #np.savetxt('Y_test.csv', Y_test)
+        #np.savetxt('preds.csv', preds)
+        #sys.exit(0)
+  print "Mean RMSE: ", np.array(L_rmse).mean()
+  print "Mean corr: ", np.array(L_corr).mean()
+  print "Mean %diff: ", np.array(L_diff).mean()
 
