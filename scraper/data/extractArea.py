77b6b4042f91c1fd78aec1f9f8fc0019036658f9
Emil Janulewicz <eemilja@Emil-Janulewiczs-MacBook-Pro.local>
2014-11-24 22:51:59 -0500

    LivingArea fields are changed to Squared Feet value

---
+++ scraper/data/extractArea.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+def convert_to_sf(raw_value):
+	key_words=['met','m','M']
+	#first convert any multiplicative items
+	if (raw_value.find('x')!=-1):
+		#print 'working on ' + raw_value
+		nums=raw_value.split('x')
+		num1=float(nums[0])
+		rest=nums[1].split()
+		num2=float(rest[0])
+		square_f=num1*num2
+		print raw_value + ' converted to ' + str(square_f) + ' '+ rest[1]
+		raw_value=str(square_f) + ' ' + rest[1]
+
+	#everything is in the squared format. put meters to feet
+	words = raw_value.split()
+	SF=float(words[0].replace(",",""))
+	for val in key_words:
+		if (raw_value.find(val)!=-1):
+			#convert to squared feet
+			SF =SF*10.7639
+			break
+
+	print raw_value + ' converted to ' + str(SF)
+	return SF
+
+data = pd.read_csv('extra_data.csv')
+#print data
+i=0
+for row in data.values:
+	row[1]=convert_to_sf(str(row[1]))
+	#print 'this is item ' + str(data['LivingArea'][i])
+	data['LivingArea'][i]=row[1]
+	i=i+1
+	
+
+data.to_csv('post_extra_data.csv')
